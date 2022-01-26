a0388b5c73f86971d721cc7d835a7a44f541078e
Jai4240 <4240@holbertonschool.com>
2022-01-25 19:08:18 -0800

    lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - main determines if number is pos, 0, or neg.
-* %d: points to the number 
+* main - main determines if number is pos, 0, or neg
+* %d: points to the number
 * Return: returns 0 for success
 */
 int main(void)
@@ -13,9 +13,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit > 5)
-{   
+{
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-}  
+}
 else if (lastDigit == 0)
 {
 printf("last digit of %d is %d and is 0/n", n, lastDigit);
