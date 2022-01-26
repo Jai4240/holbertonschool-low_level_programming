c7035cca3636418afa3959ea68e935d100a334a0
Jai4240 <4240@holbertonschool.com>
2022-01-25 19:20:48 -0800

    lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit == 0)
 {
-printf("Last digit of %d is %d and is 0", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 else
 {
