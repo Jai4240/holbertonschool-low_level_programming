f40f1475e7b88640b0b7945db6b0f3200891cab7
Jai4240 <4240@holbertonschool.com>
2022-01-26 21:50:23 -0800

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+/**
+ * Main - Main program start up
+ * point is to display the putchar and follow 
+ * with a new line.
+ **/
+
+#include "main.h"
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
