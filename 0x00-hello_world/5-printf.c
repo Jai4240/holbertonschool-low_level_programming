483cb996b80947a0d2f9ad70f78b19137fe9fd9d
Jai4240 <4240@holbertonschool.com>
2022-02-03 14:32:14 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
 * Description: prints the exact phrase followed by a new line)?
 * Return: return (0) is the required function signature
 */
-      
+
 #include <stdio.h>
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
