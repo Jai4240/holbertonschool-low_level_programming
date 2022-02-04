cc3a027316e6fda725fb1eb9c17587c8ed4fab28
Jai4240 <4240@holbertonschool.com>
2022-02-03 21:41:13 -0800

    files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+#endif /* MAIN_H */
