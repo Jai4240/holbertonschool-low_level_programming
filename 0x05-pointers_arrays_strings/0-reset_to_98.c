1f2cace1e7e678bbe8c04387732ea92700ddb497
Jai4240 <4240@holbertonschool.com>
2022-02-03 21:52:38 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+ * reset_to_98 - program start up
+ * @n: pointer being evaluated
+ *
+ *takes a pointer to an 'int"
+ *updates value to 98
+ *returns: (0) 
+ */
+
+#include "main.h"
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
