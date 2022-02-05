043880ca42a0f100533ec239788aa25437f42702
Jai4240 <4240@holbertonschool.com>
2022-02-04 21:22:44 -0800

    changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,15 @@
 #include "main.h"
-
 /**
-* print_numbers - program startup
-*
-* Description: prints the numbers `0` to `9` followed by a new line
-* Return: return (0) is the required function signature
-*/
-
+ * print_numbers - void code
+ * Return: 0
+ */
 void print_numbers(void)
 {
-	int number;
-
-	for (number = '0'; number <= '9'; number++)
-	{
-		_putchar(number);
-	}
-	_putchar('\n');
+char x;
+for (x = '0'; x <= '9'; x++)
+{
+_putchar(x);
+}
+{
+_putchar('\n');
 }
