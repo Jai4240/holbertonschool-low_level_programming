00717d7a14d4172117ea8af09cb2149f228678fe
Jai4240 <4240@holbertonschool.com>
2022-02-04 21:53:41 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - program startup
+(*
+ * Description: prints ten times the numbers `0` to `14`
+ * followed by a new line)?
+ * Return: return (0) is the required function signature
+ */
+
+void more_numbers(void)
+{
+	int stepper;
+	int number;
+
+	for (stepper = 0; stepper <= 9; stepper++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			if (number > 9)
+			{
+				_putchar((number / 10) + '0');
+			}
+			_putchar((number % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
