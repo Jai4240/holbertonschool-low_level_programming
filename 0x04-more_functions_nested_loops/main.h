2203f7b2d2f09c9b73dfce9a973c1c002214a4e5
Jai4240 <4240@holbertonschool.com>
2022-02-04 21:11:24 -0800

    gcc prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -14,5 +14,8 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+	
 #endif /* MAIN_H */
