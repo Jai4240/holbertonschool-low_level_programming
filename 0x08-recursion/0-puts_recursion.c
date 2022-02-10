bd68a29f7405be41209b9ef46ca865874081c163
Jai4240 <4240@holbertonschool.com>
2022-02-09 21:16:56 -0800

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 #include "main.h"
 void _puts_recursion(char *s)
 {
-  if (*s != '\0')
+  if (*s)
     {
       _putchar(*s);
-      s++;
-      _puts_recursion(s);
+      
+      _puts_recursion(++s);
     }
   else
     {
