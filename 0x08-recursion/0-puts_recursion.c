68236a2e24e11ba76ed6316379f6c5ee3ffe05a0
Jai4240 <4240@holbertonschool.com>
2022-02-09 21:02:25 -0800

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _puts_recursion - program startup
+ * @s: pointer that is being evaluated.
+ *
+ * Description: prints a string followed by a n.
+ * Return: (0) is the required function.
+ */
+
+#include "main.h"
+void _puts_recursion(char *s)
+{
+  if (*s != '\0')
+    {
+      _putchar(*s);
+      s++;
+      _puts_recursion(s);
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
+
