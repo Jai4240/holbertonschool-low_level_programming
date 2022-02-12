f4a65858d222bdd345b87b114ec01f472ad8cf02
Jai4240 <4240@holbertonschool.com>
2022-02-12 14:08:26 -0800

    files

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - program startup
+ * @a: pointer being evaluated
+ * @n: integer being evaluated
+(*
+ * Description: reverses the content of an array of integers)?
+ * Return: return (0) is the required function signature
+ */
+
+void reverse_array(int *a, int n)
+{
+	int string;
+	int reverse;
+
+	for (string = n - 1; string >= n / 2; string--)
+	{
+		reverse = a[string];
+		a[string] = a[n - 1 - string];
+		a[n - 1 - string] = reverse;
+	}
+}
