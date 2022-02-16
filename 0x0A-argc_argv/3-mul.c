d9457bab57292f0531b82bdf693e33e18c0881b6
Jai4240 <4240@holbertonschool.com>
2022-02-15 18:48:35 -0800

    files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry Point
+  * @argc: number of arguments
+  * @argv: vector of arguments
+  * Return: int
+  */
+int main(int argc, char **argv)
+{
+	int i, product = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		product *= atoi(argv[i]);
+	}
+	printf("%d\n", product);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry Point
+  * @argc: number of arguments
+  * @argv: vector of arguments
+  * Return: int
+  */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+
+	/* arguments < 2 means no result */
+	if (argc < 2)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		/* loop for each character in each argv */
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			/* check if character is not digit */
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		/* Assuming no errors, sum the arguments */
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
