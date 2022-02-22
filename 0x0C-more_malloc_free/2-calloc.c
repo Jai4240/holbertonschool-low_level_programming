8afcd7b45fd092c308202f9bd5833187c7b3e4e8
Jai4240 <4240@holbertonschool.com>
2022-02-21 21:38:02 -0800

    push

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stdlib.h>
 /**
- * _calloc -
- *
- * Return:
- */
+  * _calloc - malloc but with zeroes
+  * @nmemb: n members
+  * @size: size of type
+  * Return: void *
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned int n;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (n = 0; n < nmemb * size; n++)
+		*((char *)ptr + n) = 0;
+	return (ptr);
 }
